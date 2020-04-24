cce312d6726c3770cc67c770394fb793fe350ae3
dreamdroiddev <dreamdroiddev@gmail.com>
2020-04-24 14:28:01 +0600

    Follow and unflow user feature added

--- app/src/main/java/com/example/twitterfinal/TweetUsers.java
+++ app/src/main/java/com/example/twitterfinal/TweetUsers.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,14 +21,16 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TweetUsers extends AppCompatActivity {
+public class TweetUsers extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private Toolbar mToolbar;
     private ListView mListView;
+    private ArrayList<String> users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,26 +39,41 @@ public class TweetUsers extends AppCompatActivity {
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         gettingAllUserFromParse();
+        mListView.setOnItemClickListener(this);
     }
 
     private void gettingAllUserFromParse() {
-        final ArrayList<String> users = new ArrayList<>();
+        users = new ArrayList<>();
         ParseQuery<ParseUser> allUser = ParseUser.getQuery();
         allUser.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
         allUser.findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> objects, ParseException e) {
-                if (objects.size()>0 && e==null){
-                    for (ParseUser user: objects){
-                       users.add(user.getUsername());
+                try {
+                    if (objects.size()>0 && e==null){
+                        for (ParseUser user: objects){
+                            users.add(user.getUsername());
+                        }
+                    }else{
+                        Toast.makeText(TweetUsers.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        ParseUser.logOut();
                     }
-                }else{
-                    Toast.makeText(TweetUsers.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    ParseUser.logOut();
+                    mListView.setAdapter(new ArrayAdapter<String>(TweetUsers.this,android.R.layout.simple_list_item_checked,
+                            android.R.id.text1, users));
+                    mListView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+                    for (String mUser : users){
+                        if (ParseUser.getCurrentUser().getList("fanOf")!=null) {
+
+                            if (ParseUser.getCurrentUser().getList("fanOf").contains(mUser)) {
+                                mListView.setItemChecked(users.indexOf(mUser), true);
+                            }
+
+                        }
+
+                    }
+                }catch (Exception i){
+                    i.printStackTrace();
                 }
-                mListView.setAdapter(new ArrayAdapter<String>(TweetUsers.this,android.R.layout.simple_list_item_checked,
-                        android.R.id.text1, users));
-                mListView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
             }
         });
     }
@@ -78,5 +97,32 @@ public class TweetUsers extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        CheckedTextView checkedTextView = (CheckedTextView) view;
+        if (checkedTextView.isChecked()){
+            Toast.makeText(this, users.get(position)+" is followed", Toast.LENGTH_SHORT).show();
+            ParseUser.getCurrentUser().add("fanOf",users.get(position));
+        }else{
+            Toast.makeText(this, users.get(position)+" is unfollowed", Toast.LENGTH_SHORT).show();
+            ParseUser.getCurrentUser().getList("fanOf").remove(users.get(position));
+            List fanFollowedList = ParseUser.getCurrentUser().getList("fanOf");
+            ParseUser.getCurrentUser().remove("fanOf");
+            ParseUser.getCurrentUser().put("fanOf",fanFollowedList);
+        }
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Toast.makeText(TweetUsers.this, "Saved", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(TweetUsers.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
